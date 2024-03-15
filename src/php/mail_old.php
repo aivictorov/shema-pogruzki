d4216fefac0433e168eb70b56f67391b9d43874c
aivictorov <aivictorov@yandex.ru>
2024-03-15 11:07:02 +0300

    15.03.2024_1

--- src/php/mail_old.php
+++ src/php/mail_old.php
@@ -1,58 +1,97 @@
 <?php
+$to = "aivictorov@yandex.ru";
+$from_name = "Схемы погрузки (ИП Викторов)";
+$from_email = "no-reply@shema-pogruzki.ru";
+$subject_text = "Заявка с сайта";
+$message_text = "";
+$secret = '6LcXOZkpAAAAAEOdaE7jlHtaKWIvoavt_0IekIwR';
 
-$project_name = "shema";
-$admin_email = "aivictorov@yandex.ru";
-$email_from = "info@shema-pogruzki.ru";
-$form_subject = 'Заявка с сайта';
-$message = "";
+$from = '=?UTF-8?B?' . base64_encode($from_name) . '?=' . ' <' . $from_email . '>';
 
-// print_r($_FILES);
+foreach ($_POST as $key => $value) {
+	if (
+		$key != 'g-recaptcha-response' &&
+		$key != "file" &&
+		$value != ""
+	) {
+		$message_text .= $key . " " . $value . "<br>";
+	};
+};
 
-function adopt($text)
+if (!empty($_POST['g-recaptcha-response'])) {
+	$out = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+	$out = json_decode($out);
+
+	if ($out->success == true) {
+		$result = sendMailWithAttachment($to, $from, $subject_text, $message_text);
+
+		if ($result == true) {
+			echo "Спасибо! Мы свяжемся с вами в ближайшее время.";
+		} else {
+			echo "Извините, письмо не отправлено. Попробуйте еще раз.";
+		};
+	} else {
+		echo "Ошибка заполнения капчи.";
+	}
+};
+
+function sendMailWithAttachment($to, $from, $subject_text, $message_text)
 {
-	return '=?UTF-8?B?' . base64_encode($text) . '?=';
-}
-
-$fromN = adopt($project_name) . ' <' . $email_from . '>';
-
-function sendMailAttachment($mailTo, $From, $subject_text, $message)
-{
-	$to = $mailTo;
-
-	$EOL = "\r\n"; // ограничитель строк, некоторые почтовые сервера требуют \n - подобрать опытным путём
-	$boundary     = "--" . md5(uniqid(time()));  // любая строка, которой не будет ниже в потоке данных. 
-
+	$EOL = "\r\n";
+	$boundary     = "--" . md5(uniqid(time()));
 	$subject = '=?utf-8?B?' . base64_encode($subject_text) . '?=';
-
 	$headers    = "MIME-Version: 1.0;$EOL";
 	$headers   .= "Content-Type: multipart/mixed; boundary=\"$boundary\"$EOL";
-	$headers   .= "From: $From\nReply-To: $From\n";
-
+	$headers   .= "From: $from\nReply-To: $from\n";
 	$multipart  = "--$boundary$EOL";
 	$multipart .= "Content-Type: text/html; charset=utf-8$EOL";
 	$multipart .= "Content-Transfer-Encoding: base64$EOL";
-	$multipart .= $EOL; // раздел между заголовками и телом html-части 
-	$multipart .= chunk_split(base64_encode($message));
+	$multipart .= $EOL;
+	$multipart .= chunk_split(base64_encode($message_text));
 
-	#начало вставки файлов
-	if (!empty($_FILES['file']['name'][0])) {
+	if (!empty($_FILES["file"]['name'][0])) {
+
 		foreach ($_FILES["file"]["name"] as $key => $value) {
-			$filename = $_FILES["file"]["tmp_name"][$key];
-			$file = fopen($filename, "rb");
-			$data = fread($file,  filesize($filename));
-			fclose($file);
-			$NameFile = $_FILES["file"]["name"][$key]; // в этой переменной надо сформировать имя файла (без всякого пути);
-			$File = $data;
-			$multipart .=  "$EOL--$boundary$EOL";
-			$multipart .= "Content-Type: application/octet-stream; name=\"$NameFile\"$EOL";
-			$multipart .= "Content-Transfer-Encoding: base64$EOL";
-			$multipart .= "Content-Disposition: attachment; filename=\"$NameFile\"$EOL";
-			$multipart .= $EOL; // раздел между заголовками и телом прикрепленного файла 
-			$multipart .= chunk_split(base64_encode($File));
-		};
-	}
-	#>>конец вставки файлов
 
+			$filename = $_FILES["file"]["tmp_name"][$key];
+			$fileSize = $_FILES["file"]["size"][$key];
+
+			if ($fileSize > (5 * 1024 * 1024)) {
+				echo 'Слишком большой файл.';
+			}
+
+			echo "<br>";
+			print_r($_FILES["file"]["name"][$key]);
+			echo "<br>";
+			print_r(preg_match("/\.(jpg|jpeg|png|pdf)$/i", $_FILES["file"]["name"][$key]));
+			echo "<br>";
+			print_r($filename);
+			echo "<br>";
+			print_r(mime_content_type($filename));
+			echo "<br>";
+
+			if (
+				preg_match("/\.(jpg|jpeg|png|pdf)$/i", $_FILES["file"]["name"][$key]) ||
+				mime_content_type($filename) == "image/jpeg" ||
+				mime_content_type($filename) == "image/png" ||
+				mime_content_type($filename) == "application/pdf"
+			) {
+				$file = fopen($filename, "rb");
+				$data = fread($file,  filesize($filename));
+				fclose($file);
+				$NameFile = $_FILES["file"]["name"][$key];
+				$File = $data;
+				$multipart .=  "$EOL--$boundary$EOL";
+				$multipart .= "Content-Type: application/octet-stream; name=\"$NameFile\"$EOL";
+				$multipart .= "Content-Transfer-Encoding: base64$EOL";
+				$multipart .= "Content-Disposition: attachment; filename=\"$NameFile\"$EOL";
+				$multipart .= $EOL;
+				$multipart .= chunk_split(base64_encode($File));
+			} else {
+				echo 'Неверный формат файла.';
+			};
+		};
+	};
 
 	$multipart .= "$EOL--$boundary--$EOL";
 
@@ -61,39 +100,4 @@ function sendMailAttachment($mailTo, $From, $subject_text, $message)
 	} else {
 		return true;
 	};
-};
-
-foreach ($_POST as $key => $value) {
-	if ($value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" && $key != "email_from" && $key != 'g-recaptcha-response' && $key != "file") {
-		$message .= $key . " " . $value . "<br>";
-	};
-}
-
-
-
-// $headers =
-// 	"MIME-Version: 1.0" . PHP_EOL .
-// 	"Content-Type: text/html; charset=utf-8" . PHP_EOL .
-// 	// "Content-Type: multipart/form-data; charset=utf-8" . PHP_EOL .
-// 	'From: ' . adopt($project_name) . ' <' . $email_from . '>' . PHP_EOL .
-// 	'Reply-To: ' . $admin_email . '' . PHP_EOL;
-
-
-$secret = '6LcXOZkpAAAAAEOdaE7jlHtaKWIvoavt_0IekIwR';
-
-if (!empty($_POST['g-recaptcha-response'])) {
-	$out = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
-	$out = json_decode($out);
-	if ($out->success == true) {
-		$result = sendMailAttachment($admin_email, $fromN, $form_subject, $message);
-		// $result = mail($admin_email, $form_subject, $message, $headers);
-	} else {
-		echo 'Ошибка заполнения капчи.';
-	}
-};
-
-if ($result == true) {
-	echo 'Спасибо! Мы свяжемся с вами в ближайшее время.';
-} else {
-	echo 'Извините, письмо не отправлено. Попробуйте еще раз.';
 };
